144bfd9d47b3ded49c000bd7df3d8865e6cb8e70
Matesor333 <matus.illit@gmail.com>
2026-02-28 19:06:09 +0000

    Hopefully fixed

--- api/db.php
+++ api/db.php
@@ -4,10 +4,10 @@ header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: Content-Type");
 
 // Database credentials
-$host = 'localhost'; // Usually localhost on WebSupport
-$db   = 'treeroute_db'; // Update with your actual database name
+$host = 'db.r6.websupport.sk'; // Usually localhost on WebSupport
+$db   = 'Hackthon'; // Update with your actual database name
 $user = 'Hackthon';
-$pass = 'YOUR_PASSWORD_HERE'; // Replace on your actual server!
+$pass = 'FuckHack123='; // Replace on your actual server!
 $charset = 'utf8mb4';
 
 $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
